f416d0b9fac98229043846a26178dec5be839ba5
chani0548558941@gmail.com <chani0548558941@gmail.com>
2025-03-12 16:45:07 +0200

    build ok??!!

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: '',
+    renderMode: RenderMode.Server, // הוספת נתיב לשורש
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Server // הוספת נתיב ל-login
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Server // הוספת נתיב ל-register
+  },
+  {
+    path: 'navbar',
+    renderMode: RenderMode.Server // הוספת נתיב ל-navbar
+  },
+  {
+    path: 'navbar/:id',
+    renderMode: RenderMode.Server // נתיב עבור ShowCourseComponent
+  },
+  {
+    path: 'navbar/AddLesson/:id',
+    renderMode: RenderMode.Server // נתיב עבור AddLessonComponent
+  },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,14 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { AddLessonComponent } from '../components/add-lesson/add-lesson.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { HomePageComponent } from '../components/home-page/home-page.component';
-import { PrerenderParamsResolver } from './getPrerenderParams';
+import { of } from 'rxjs';
+import { ActivatedRouteSnapshot } from '@angular/router';
+
+// פונקציה להחזרת פרמטרים עבור prerendering
+export function getPrerenderParams(route: ActivatedRouteSnapshot) {
+    const id = route.paramMap.get('id');
+    return of({ id }); // מחזיר את הפרמטרים הנדרשים
+}
 
 export const routes: Routes = [
     { path: '', component: HeaderComponent },
@@ -19,10 +26,10 @@ export const routes: Routes = [
         path: 'navbar', component: NavbarComponent,
         children: [
             { path: 'courses', component: CoursesComponent },
-            { path: 'courses/:id', component: ShowCourseComponent, resolve: { params: PrerenderParamsResolver }, data: { renderMode: 'default' } },
             { path: 'home', component: HomePageComponent },
-            { path: 'addCourse', component: AddTeacherComponent, canActivate: [teacherGuard], data: { renderMode: 'default' }},
-            { path: 'AddLesson/:id', component: AddLessonComponent, resolve: { params: PrerenderParamsResolver }, data: { renderMode: 'default' }}
+            { path: 'addCourse', component: AddTeacherComponent, canActivate: [teacherGuard] },
+            { path: ':id', component: ShowCourseComponent, resolve: { params: getPrerenderParams } },
+            { path: 'AddLesson/:id', component: AddLessonComponent, resolve: { params: getPrerenderParams } }
         ]
-    }
+    },
 ];
